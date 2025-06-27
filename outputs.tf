e5654402dc66242596fc4f5ace0aa2faff43ece7
Naveen <naveenramlu@gmail.com>
2025-06-28 02:58:23 +0530

    updated the deploy

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,5 @@
 output "s3_website_url" {
   description = "The URL of the S3 static website"
-  value       = aws_s3_bucket_website_configuration.example.website_endpoint
+  #value       = aws_s3_bucket_website_configuration.example.website_endpoint
+  value = aws_s3_bucket.example.bucket
 }
